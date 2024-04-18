10c33fe48739b0b7211fa8b0cd6cabc4f6908eb7
chandu <chandanayadavdolly@gmail.com>
2024-04-18 12:06:01 +0530

    added matplotlib

---
+++ Machine learning/Matplotlib/2_plot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt #for visualization
+import numpy as np #for array operation
+
+x=np.array([0,6])
+y=np.array([0,10])
+
+plt.plot(x,y,'-o')#creating plot
+plt.show()#display the plot
